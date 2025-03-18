3fb08a0cca9f67b81f8cb6e754aa78e686a7791b
maradw <maria.mendoza@tecsup.edu.pe>
2025-03-18 17:40:30 -0500

    base

---
+++ Assets/Week1/ClassTouch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Week1
+{
+    public class ClassTouch : MonoBehaviour
+    {
+        [SerializeField] GameObject[] _colors;
+        [SerializeField] GameObject[] _images;
+        Color _currentColor;
+        Sprite _curretSprite;
+
+        // Use this for initialization
+        void Start()
+        {
+            for (int i = 0; i < _colors.Length; i++) 
+            {
+                //_colors[i].tintColor = Color.white;
+            }
+
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            Instantiate(_images[1]);
+            if (Input.touchCount > 0)
+            {
+               // Instantiate(_images[1]);
+                Touch currentTouch = Input.GetTouch(0);
+                Instantiate(_images[1]);
+                if (currentTouch.phase == TouchPhase.Began)
+                {
+                    Debug.Log("Touch");
+
+                }
+            }
+        }
+        void InstantiateImage()
+        {
+            Instantiate(_images[1]);
+        }
+    }
+}
