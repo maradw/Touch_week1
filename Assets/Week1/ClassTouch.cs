4b7997207a6a6c2de0f5b744d368b38bb89ce182
maradw <maria.mendoza@tecsup.edu.pe>
2025-03-20 15:47:03 -0500

    buttons

--- Assets/Week1/ClassTouch.cs
+++ Assets/Week1/ClassTouch.cs
@@ -8,38 +8,58 @@ namespace Assets.Week1
     {
         [SerializeField] GameObject[] _colors;
         [SerializeField] GameObject[] _images;
+        [SerializeField] public Image mySpr;
+        private Button oño;
+
         Color _currentColor;
-        Sprite _curretSprite;
+        //SpriteTG  _curretSprite;
 
         // Use this for initialization
         void Start()
         {
+            oño = GetComponent<Button>();
+
+            Debug.Log("NOSE");
             for (int i = 0; i < _colors.Length; i++) 
             {
                 //_colors[i].tintColor = Color.white;
             }
+           // Debug.Log("ola" + mySpr.name);
 
         }
         
         // Update is called once per frame
         void Update()
         {
-            Instantiate(_images[1]);
+           // Instantiate(_images[1]);
             if (Input.touchCount > 0)
             {
                // Instantiate(_images[1]);
                 Touch currentTouch = Input.GetTouch(0);
-                Instantiate(_images[1]);
+                //Instantiate(_images[1]);
                 if (currentTouch.phase == TouchPhase.Began)
                 {
-                    Debug.Log("Touch");
+                   // Debug.Log("Touch");
 
                 }
             }
+            
         }
         void InstantiateImage()
         {
             Instantiate(_images[1]);
+        }
+        public void GetImage()
+        {
+            if(mySpr == null)
+            {
+                mySpr = _images[1].GetComponent<Image>();
+                Debug.Log("cambio?" + mySpr.name);
+                
+            }
+            //mySpr = _images[1].GetComponent<Image>() ;
+            //Debug.Log("cambio?" + mySpr.name);
+
         }
     }
 }
